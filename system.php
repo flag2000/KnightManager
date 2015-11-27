8f9ade7c5825fa4e376e3874efdf3cb8a5c02d50
flag2000 <bornemph@gmx.de>
2015-11-27 17:41:37 +0100

    system.php erstellt

---
+++ system.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author	Philipp Bornemann
+ * @copyright	2015-2017 Philipp Bornemann
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+public $version = "1.0.0 Pre-Alpha 1";
+public $version_code = 0100;
+public $status = 0; // 0 = unstable, 1 = stable
+?>
