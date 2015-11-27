97a1c480ef037677505eed7696eb5d6ee6b4cde2
flag2000 <bornemph@gmx.de>
2015-11-27 18:37:00 +0100

    Änderungen an system.php

--- system.php
+++ system.php
@@ -4,7 +4,18 @@
  * @copyright	2015-2017 Philipp Bornemann
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
-public $version = "1.0.0 Pre-Alpha 1";
-public $version_code = 0100;
-public $status = 0; // 0 = unstable, 1 = stable
+class KnightManager {
+	public function show_version() {
+		$version = "1.0.0 Pre-Alpha 1";
+		echo $version;
+	}
+	public function show_version_code() {
+		$version_code = 0100;
+		echo $version_code;
+	}
+	public function show_status() {
+		$status = 0;
+		// Wenn Status = 0, soll eine Meldung ausgegeben werden, dass sich die Software in der Testphase befindet
+	}
+}
 ?>
