d692e786bea029d1fee62fa28742dafa525700ca
flag2000 <bornemph@gmx.de>
2015-11-29 14:53:37 +0100

    ACP Login

---
+++ acp/login.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @author	Philipp Bornemann
+ * @copyright	2015-2017 Philipp Bornemann
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+require_once('../template/acp_login.html');
+?>
