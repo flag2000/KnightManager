a31330985a0b56ad552b8459ee6e8f12498fd15f
flag2000 <bornemph@gmx.de>
2015-12-04 18:46:55 +0100

    Vorbereitung

--- install.sql
+++ install.sql
@@ -1,7 +1,7 @@
 CREATE TABLE km_user (
 	userID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(255) NOT NULL DEFAULT '',
-	password VARCHAR(150) NOT NULL DEFAULT '',
+	password VARCHAR(255) NOT NULL DEFAULT '',
 	email VARCHAR(255) NOT NULL DEFAULT '',
 	activated TINYINT(1) NOT NULL DEFAULT 0,
 	user_group INT(10),
@@ -13,6 +13,8 @@ CREATE TABLE km_user_group (
 	groupID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	group_name VARCHAR(255) NOT NULL DEFAULT ''
 );
+
+INSERT INTO km_user (userID, username, passwort, email, activated, user_group, first_name, name) VALUES (1, 'System', '$2y$10$k40Ct3Spk2pE0QkWM36EWeNhmTkda9g6V0eS9ZsKc2k6Btjls159e', 'test@test.de', 0, 1, 'System', 'System');
 
 INSERT INTO km_user_group (groupID, group_name) VALUES (1, 'Administrator');
 INSERT INTO km_user_group (groupID, group_name) VALUES (2, 'Moderator');
