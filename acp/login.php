fab9115a0afc384de0c599c88f385384b68c7208
flag2000 <bornemph@gmx.de>
2015-12-05 14:17:22 +0100

    Änderungen

--- acp/login.php
+++ acp/login.php
@@ -4,5 +4,13 @@
  * @copyright	2015-2017 Philipp Bornemann
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
+require_once('../KnightManager.class.php');
 require_once('../template/acp_login.html');
+
+$username = $_POST['inputUsername'];
+$email = $_POST['inputUsername'];
+$password = password_hash($_POST['inputPassword'], PASSWORD_BCRYPT);
+
+$login = new KnightManager;
+$login->login_acp($username, $email, $password);
 ?>
